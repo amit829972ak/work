2e93d39b6e9c1bde2677e3fddc5691ab3e2002e9
Amit Kumar <127661512+amit829972ak@users.noreply.github.com>
2023-09-16 20:15:16 +0530

    Update e.py

--- e.py
+++ e.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 import PyPDF2
 from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 # Get the current directory (where this script is located)
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -14,6 +16,9 @@ st.title("List of Folders in Current Directory")
 if folder_names:
     selected_pdfs = []
 
+    # Create a mapping of selected PDFs to their folder and file paths
+    selected_pdf_paths = []
+
     for folder in folder_names:
         # List PDF files in the current folder
         folder_path = os.path.join(current_directory, folder)
@@ -23,26 +28,61 @@ if folder_names:
         pdf_file_names = [os.path.basename(pdf_file) for pdf_file in pdf_files]
 
         # Create a multiselect widget to choose PDF files for the current folder
-        selected_pdfs.extend(st.multiselect(f"Select PDFs for '{folder}':", pdf_file_names))
+        selected_pdf_names = st.multiselect(f"Select PDFs for '{folder}':", pdf_file_names)
 
-    if selected_pdfs:
+        # Map selected PDFs to their folder and file paths
+        for selected_pdf_name in selected_pdf_names:
+            selected_pdf_paths.append((folder, selected_pdf_name))
+
+    if selected_pdf_paths:
         st.write("Selected PDFs:")
-        for pdf_name in selected_pdfs:
+        for folder, pdf_name in selected_pdf_paths:
             st.write(pdf_name)
 
-        # Merge the selected PDFs into a single PDF
+        # Create an "Index" PDF page with selected PDFs mentioned in order
+        index_pdf_bytes = BytesIO()
+        c = canvas.Canvas(index_pdf_bytes, pagesize=letter)
+        c.setFont("Helvetica-Bold", 14)
+        c.drawString(100, 400, "Index")
+        c.setFont("Helvetica", 12)
+        y_position = 380
+        for i, (_, pdf_name) in enumerate(selected_pdf_paths, start=1):
+            c.drawString(100, y_position - i * 20, f"{i}. {pdf_name}")
+        c.showPage()
+        c.save()
+
+        # Merge the selected PDFs and the "Index" PDF
         merged_pdf = PyPDF2.PdfMerger()
-        for pdf_name in selected_pdfs:
+
+        # Append the first two selected PDFs
+        for i in range(2):
+            if i < len(selected_pdf_paths):
+                folder, pdf_name = selected_pdf_paths[i]
+                # Get the full path of the selected PDF using its name
+                folder_path = os.path.join(current_directory, folder)
+                pdf_path = os.path.join(folder_path, pdf_name)
+                merged_pdf.append(pdf_path)
+
+        # Insert the "Index" PDF as the third page
+        index_pdf_bytes.seek(0)
+        
+        merged_pdf.append(index_pdf_bytes)
+
+        # Append the rest of the selected PDFs
+        for i in range(2, len(selected_pdf_paths)):
+            folder, pdf_name = selected_pdf_paths[i]
             # Get the full path of the selected PDF using its name
-            folder_index = selected_pdfs.index(pdf_name)
-            pdf_path = os.path.join(current_directory, folder_names[folder_index], pdf_files[folder_index])
+            folder_path = os.path.join(current_directory, folder)
+            pdf_path = os.path.join(folder_path, pdf_name)
             merged_pdf.append(pdf_path)
 
         # Create a download link for the merged PDF
         st.markdown("### Download Merged PDF")
+        
         with st.spinner("Merging PDFs..."):
             merged_pdf_bytes = BytesIO()
             merged_pdf.write(merged_pdf_bytes)
+            
             st.download_button("Download Merged PDF", merged_pdf_bytes.getvalue(), file_name="merged_pdf.pdf", key="merged_pdf")
 
     else:
